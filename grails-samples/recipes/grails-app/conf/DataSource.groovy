a3c4a51cd831aeb26a1ab0d90c6afe3077548ab6
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-08-08 12:17:47 +0000

    changed data source format
    git-svn-id: https://svn.codehaus.org/grails/trunk@5114 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

---
+++ grails-samples/recipes/grails-app/conf/DataSource.groovy
@@ -0,0 +1,28 @@
+dataSource {
+	pooled = false
+	driverClassName = "org.hsqldb.jdbcDriver"
+	username = "sa"
+	password = ""  
+	configClass = org.codehaus.groovy.grails.orm.hibernate.cfg.GrailsAnnotationConfiguration
+}
+// environment specific settings
+environments {
+	development {
+		dataSource {
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:mem:devDB"
+		}
+	}
+	test {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:mem:testDb"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+		}
+	}
+}
