368dcf9c0204d4a69a91d46e1db091a81014ae42
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2007-10-18 02:00:58 +0000

    first implementation of reference navigation and user guide split
    git-svn-id: https://svn.codehaus.org/grails/trunk@5805 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-doc/Generate.groovy
+++ grails-doc/Generate.groovy
@@ -1,6 +1,6 @@
 import com.plink.plextile.*; 
 
-files = new File("./src").listFiles().findAll { it.name.endsWith(".textile") }
+files = new File("./src/guide").listFiles().findAll { it.name.endsWith(".textile") }
 parser = new TextParser()
 ant = new AntBuilder()
 
@@ -26,9 +26,19 @@ for(entry in book) {
 
 ant.mkdir(dir:"output")                      
 ant.mkdir(dir:"output/img")                      
+ant.mkdir(dir:"output/css")   
+ant.mkdir(dir:"output/ref")         
+ant.copy(file:"resources/style/reference.html",todir:"output")
 ant.copy(todir:"output/img") {
 	fileset(dir:"resources/img")
+}       
+ant.copy(todir:"output/css") {
+	fileset(dir:"resources/css")
 }                              
+ant.copy(todir:"output/ref") {
+	fileset(dir:"resources/style/ref")
+}                              
+                       
 vars = [
             title:"Grails",
 			version: "1.0-RC2",
@@ -41,7 +51,36 @@ new File("./resources/style/layout.html").withReader { reader ->
 	new File("output/index.html").withWriter { out ->
 		template.make(vars).writeTo(out)		
 	}
-}   
+}    
+
+menu = new StringBuffer()
+files = new File("src/ref").listFiles()
+new File("resources/style/referenceItem.html").withReader { reader ->
+	template = engine.createTemplate(reader)		
+	for(f in files) {
+		if(f.directory && !f.name.startsWith(".")) {
+			menu << "<h1 class=\"menuTitle\">${f.name}</h1>"
+			def textiles = f.listFiles().findAll { it.name.endsWith(".textile")}
+			for(txt in textiles) {                        
+				def name = txt.name[0..-9]
+				menu << "<div class=\"menuItem\"><a href=\"${name}.html\" target=\"mainFrame\">${name}</a></div>"
+				def contents = parser.parseTextile(txt.text, true)			
+				
+				new File("output/ref/${name}.html").withWriter { out ->
+				 	template.make(content:contents).writeTo(out)
+				}
+			}
+		}
+	}        
+	
+}
+vars.menu = menu
+new File("./resources/style/menu.html").withReader { reader ->
+	template = engine.createTemplate(reader)	
+	new File("output/ref/menu.html").withWriter { out ->
+		template.make(vars).writeTo(out)		
+	}
+}
 println "Done. Look at output/index.html"
 
 
