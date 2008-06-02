8565eb016e13d732704ba7bf3de2c909129e6f6d
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2008-06-02 16:33:20 +0000

    Fixed the failing functional tests and refactored the artefact reloading tests. The failures were due to the fact that the tests were modifying files and then checking for changes within 1 second. Since the underlying filesystem has a granularity of a second for timestamps, the files didn't appear to be modified!
    git-svn-id: https://svn.codehaus.org/grails/trunk@7092 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-functional-tests/apps/HelloWorld/webtest/tests/HelloControllerTest.groovy
+++ grails-functional-tests/apps/HelloWorld/webtest/tests/HelloControllerTest.groovy
@@ -45,119 +45,81 @@ class HelloControllerTest extends grails.util.WebTest {
     }
 
     def testReloadingController() {
-    	def originalText = "Hello world!"
-       	def newText = "Adios!"
-       	def f = new File("grails-app/controllers/HelloController.groovy")
-    	def originalCode = f.text
-    	def newCode = originalCode.replaceAll(originalText, newText)
-    	assert originalCode != newCode
-
-    	webtest('Controller reloading') {
-    		group(description: "Gets the page and check original output") {
-	            invoke      (url: 'hello/')
-	            verifyText text: "$originalText"
-	            not {
-	            	verifyText text: "$newText"
-	            }
-    		}
-
-            groovy description: "Modify the controller", """
-def f = new File("$f")
-f.withWriter { writer ->
-    writer << '''$newCode'''
-}
-"""
-    		group(description: "Gets the page again and check new output") {
-	            invoke      (url: 'hello/')
-            	verifyText text: "$newText"
-	            not {
-    	            verifyText text: "$originalText"
-	            }
-    		}
-        }
-
-        // Reset the controller back to the way it was.
-        f.withWriter { writer ->
-            writer << originalCode
-        }
+        testReloading(
+                new File("grails-app/controllers/HelloController.groovy"),
+                "Controller reloading",
+                "Hello world!",
+                "Adios!",
+                "hello/",
+                "")
     }
 
     def testReloadingControllerView() {
-    	def originalText = "Hello from myTag!"
-       	def newText = "new output of myTag"
-       	def f = new File("grails-app/taglib/FooTagLib.groovy")
-    	def originalCode = f.text
-    	def newCode = originalCode.replaceAll(originalText, newText)
-    	assert originalCode != newCode
-
-    	webtest('TagLib reloading') {
-    		group(description: "Gets the page and check original tag output") {
-	            invoke      (url: 'hello/reloadTest')
-	            verifyTitle  (text: 'TagLib reloading test')
-	            verifyText text: "$originalText"
-	            not {
-	            	verifyText text: "$newText"
-	            }
-    		}
-
-            groovy description: "Modify the taglib", """
-def f = new File("$f")
-f.withWriter { writer ->
-    writer << '''$newCode'''
-}
-"""
-    		group(description: "Gets the page again and check new tag output") {
-	            invoke      (url: 'hello/reloadTest')
-	            verifyTitle  (text: 'TagLib reloading test')
-            	verifyText text: "$newText"
-	            not {
-    	            verifyText text: "$originalText"
-	            }
-    		}
-        }
-
-        // Reset the tag lib back to the way it was.
-        f.withWriter { writer ->
-            writer << originalCode
-        }
+        testReloading(
+                new File("grails-app/taglib/FooTagLib.groovy"),
+                "TagLib reloading with controller view",
+                "Hello from myTag!",
+                "new output of myTag",
+                "hello/reloadTest",
+                "TagLib reloading test")
     }
 
     def testReloadingStandaloneView() {
-    	def originalText = "Hello from myTag!"
-       	def newText = "new output of myTag"
-       	def f = new File("grails-app/taglib/FooTagLib.groovy")
-    	def originalCode = f.text
+        testReloading(
+                new File("grails-app/taglib/FooTagLib.groovy"),
+                "TagLib reloading with standalone view",
+                "Hello from myTag!",
+                "new output of myTag",
+                "tagUsage.gsp",
+                "For taglib tests")
+    }
+
+    private void testReloading(
+            File artefactFile,
+            String webtestDescription,
+            String originalText,
+            String newText,
+            String invokeUrl,
+            String pageTitle) {
+    	def originalCode = artefactFile.text
     	def newCode = originalCode.replaceAll(originalText, newText)
     	assert originalCode != newCode
 
-    	webtest('TagLib reloading in with standalone view') {
-    		group(description: "Gets the page and check original tag output") {
-	            invoke      (url: 'tagUsage.gsp')
-	            verifyTitle  (text: 'For taglib tests')
-	            verifyText text: "$originalText"
+    	webtest(webtestDescription) {
+    		group(description: "Gets the page and checks original output") {
+	            invoke      (url: invokeUrl)
+	            verifyTitle (text: pageTitle)
+	            verifyText  (text: "$originalText")
 	            not {
-	            	verifyText text: "$newText"
+	            	verifyText (text: "$newText")
 	            }
     		}
-    		
-    		groovy description: "Modify the taglib", """
-def f = new File("$f")
+
+    		groovy description: "Modify the artefact", """
+// Ensures that the file appears modified on systems that only
+// have a granularity of a second on their file timestamps.
+Thread.sleep(1000)
+
+def f = new File("$artefactFile")
 f.withWriter { writer ->
     writer << '''$newCode'''
 }
 """
-    		group(description: "Gets the page again and check new tag output") {
-	            invoke      (url: 'tagUsage.gsp')
-	            verifyTitle  (text: 'For taglib tests')
-            	verifyText text: "$newText"
+    		group(description: "Gets the page again and checks new output") {
+	            invoke      (url: invokeUrl)
+	            verifyTitle (text: pageTitle)
+            	    verifyText  (text: "$newText")
 	            not {
-    	            verifyText text: "$originalText"
+    	                verifyText (text: "$originalText")
 	            }
     		}
         }
 
-        // Reset the tag lib back to the way it was.
-        f.withWriter { writer ->
+        // Reset the tag lib back to the way it was. The "sleep()"
+        // ensures that the file appears modified on systems that only
+        // have a granularity of a second on their file timestamps.
+        Thread.sleep(1000)
+        artefactFile.withWriter { writer ->
             writer << originalCode
         }
     }
