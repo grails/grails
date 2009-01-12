b6f6c15df6412baf4415e087a9ebf1d3d1c2f8d8
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-01-12 14:30:30 +0000

    changed to static allowedMethods to avoid deprecation message
    git-svn-id: https://svn.codehaus.org/grails/trunk@8128 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-samples/dgg/Chapter02/grails-app/controllers/com/g2one/gtunes/AlbumController.groovy
+++ grails-samples/dgg/Chapter02/grails-app/controllers/com/g2one/gtunes/AlbumController.groovy
@@ -5,7 +5,7 @@ class AlbumController {
     def index = { redirect(action:list,params:params) }
 
     // the delete, save and update actions only accept POST requests
-    def allowedMethods = [delete:'POST', save:'POST', update:'POST']
+    static allowedMethods = [delete:'POST', save:'POST', update:'POST']
 
     def list = {
         if(!params.max) params.max = 10
