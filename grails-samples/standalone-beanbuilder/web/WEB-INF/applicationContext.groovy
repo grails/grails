84087246c894107969c3d2b7caafc8f267808740
graeme <graeme@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-01-16 10:51:17 +0000

    defined the beans in BeanBuilder instead
    git-svn-id: https://svn.codehaus.org/grails/trunk@8201 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-samples/standalone-beanbuilder/web/WEB-INF/applicationContext.groovy
+++ grails-samples/standalone-beanbuilder/web/WEB-INF/applicationContext.groovy
@@ -1,3 +1,8 @@
+import org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping
+import com.example.mvc.WelcomeController
+
 beans {
     hello(String, "hello world")
+    handlerMapping(ControllerClassNameHandlerMapping)
+    welcomeController(WelcomeController)
 }
