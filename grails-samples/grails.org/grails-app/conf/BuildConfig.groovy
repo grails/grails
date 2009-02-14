42ea8a9198787c4be4bffb7fd25ec26c429eb5fb
pledbrook <pledbrook@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-02-14 20:21:35 +0000

    Corrected the plugin directory config option.
    git-svn-id: https://svn.codehaus.org/grails/trunk@8554 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-samples/grails.org/grails-app/conf/BuildConfig.groovy
+++ grails-samples/grails.org/grails-app/conf/BuildConfig.groovy
@@ -1,3 +1,4 @@
 grails.project.work.dir = "target"
 grails.project.test.reports.dir = "target/test-reports"
-grails.plugins.dir = "plugins"
+grails.project.plugins.dir = "plugins"
+grails.plugin.location.liquibase = 'plugins/liquibase'
