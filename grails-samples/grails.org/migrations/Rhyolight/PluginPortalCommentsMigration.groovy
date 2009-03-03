1a60fa40b2b5d1ff8483dc8b42128f538fc5b644
rhyolight <rhyolight@1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d>
2009-03-03 16:27:59 +0000

    Trying a failOnError=false setting to see if the build works
    git-svn-id: https://svn.codehaus.org/grails/trunk@8805 1cfb16fd-6d17-0410-8ff1-b7e8e1e2867d

--- grails-samples/grails.org/migrations/Rhyolight/PluginPortalCommentsMigration.groovy
+++ grails-samples/grails.org/migrations/Rhyolight/PluginPortalCommentsMigration.groovy
@@ -1,4 +1,4 @@
-changeSet(id:'PluginPortalComments', author:'Rhyolight') { 
+changeSet(id:'PluginPortalComments', author:'Rhyolight', failOnError:false) {
     dropTable(tableName:'blog_entry_comment')
     dropColumn(tableName:'comment', columnName:'email')
     dropColumn(tableName:'comment', columnName:'poster')
