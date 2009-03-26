97ca78a5f8e9dcb75986dd36640d7177dfdea1a8
Matthew Taylor <rhyolight@gmail.com>
2009-03-26 10:10:44 -0500

    Making updates to the deploy script for new user permissions on the prod server.

--- grails-samples/grails.org/src/bash/deploy.sh
+++ grails-samples/grails.org/src/bash/deploy.sh
@@ -18,6 +18,10 @@ then
     deployDir='www'
 fi
 
+# either beta-j2ee or www-j2ee
+user=$deployDir-j2ee
+echo "user is $user"
+
 deployLoc="/opt/j2ee/domains/$url/$deployDir/webapps/$appName"
 echo "deployment location is $deployLoc"
 sourceLoc="$sourceRoot/apps/$url"
@@ -31,29 +35,29 @@ git pull
 cd $sourceLoc
 
 echo "Packaging grails war in $sourceLoc ..."
-grails clean
-rm *.war
-grails prod war
+# ~/grails-1.1/bin/grails clean
+# rm *.war
+# ~/grails-1.1/bin/grails prod war
 
 cdtDate=`TZ=CST date +"%Y%m%d-%H%M"`
 newFolder=$name-$version-$cdtDate
 echo "Creating new deployment to folder: $newFolder..."
 warFolder=$deployLoc/$newFolder/exploded_war
-mkdir -p $warFolder
-chown -R www-j2ee:www-j2ee $deployLoc/$newFolder
+sudo $user mkdir -p $warFolder
+sudo $user chown -R www-j2ee:www-j2ee $deployLoc/$newFolder
 cd $warFolder
 echo "Exploding war..."
 jar xf $sourceLoc/*.war
 
 echo "Copying over images from previous version..."
-cp $deployLoc/$name-$prevVersion/exploded_war/images/* $deployLoc/$newFolder/images
+sudo $user cp $deployLoc/$name-$prevVersion/exploded_war/images/* $deployLoc/$newFolder/images
 
 echo "Replacing current directory..."
 cd $deployLoc
 if (test -e "current"); then
-    rm "current"
+    sudo $user rm "current"
 fi
-ln -s $newFolder current
+sudo $user ln -s $newFolder current
 
 echo "$deployLoc/$newFolder is now ready to be deployed"
 cd $currentDir
