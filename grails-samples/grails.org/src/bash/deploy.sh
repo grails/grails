c65d7271109c6b770fe71c7550e59256c57927fa
Matthew Taylor <rhyolight@gmail.com>
2009-03-26 11:38:05 -0500

    deployment script one more time.

--- grails-samples/grails.org/src/bash/deploy.sh
+++ grails-samples/grails.org/src/bash/deploy.sh
@@ -26,9 +26,9 @@ git pull
 cd $sourceLoc
 
 echo "Packaging grails war in $sourceLoc ..."
-# ~/grails-1.1/bin/grails clean
-# rm *.war
-# ~/grails-1.1/bin/grails prod war
+~/grails-1.1/bin/grails clean
+rm *.war
+~/grails-1.1/bin/grails prod war
 
 cdtDate=`TZ=CST date +"%Y%m%d-%H%M"`
 newFolder=$name-$version-$cdtDate
@@ -36,19 +36,18 @@ echo "Creating new deployment to folder: $newFolder..."
 warFolder=$deployLoc/$newFolder/exploded_war
 mkdir -p $warFolder
 
-chown -R www-j2ee:www-j2ee $deployLoc/$newFolder
+# chown -R www-j2ee:www-j2ee $deployLoc/$newFolder
 cd $warFolder
 echo "Exploding war..."
 jar xf $sourceLoc/*.war
 
 echo "Copying over images from previous version..."
-cp $deployLoc/$name-$prevVersion/exploded_war/images/* $deployLoc/$newFolder/exploded_war/images
+cp -r $deployLoc/$name-$prevVersion/exploded_war/images/* $deployLoc/$newFolder/exploded_war/images
 
 echo "Replacing current directory..."
 cd $deployLoc
-if (test -e "current"); then
-    rm "current"
-fi
+rm previous
+mv current previous
 ln -s $newFolder current
 
 echo "$deployLoc/$newFolder is now ready to be deployed"
