63937cf53251918872137ee888ed183c9a26d93a
Jeff Brown <jeff@jeffandbetsy.net>
2009-06-09 04:51:39 -0500

    update grails-hibernate version number to 1.2-SNAPSHOT

--- grails-plugins/grails-hibernate/trunk/HibernateGrailsPlugin.groovy
+++ grails-plugins/grails-hibernate/trunk/HibernateGrailsPlugin.groovy
@@ -30,7 +30,7 @@ class HibernateGrailsPlugin {
 	def description = "A plugin that provides integration between Grails and Hibernate through GORM"
 
 	
-    def version = "1.1"
+    def version = "1.2-SNAPSHOT"
     def documentation = 'http://grails.org/doc/$version'	
 
     def dependsOn = [dataSource: "1.1 > *",
